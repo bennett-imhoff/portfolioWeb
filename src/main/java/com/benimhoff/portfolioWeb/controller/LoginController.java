63b218a50dd1a398e896546bd8fc6b5ce5a3fda2
Bennett Imhoff Anzor <simplementebennett@gmail.com>
2023-12-26 16:59:23 -0300

    Redirijo oportunamente al homeEdicion según si el usuario es valido o no

--- src/main/java/com/benimhoff/portfolioWeb/controller/LoginController.java
+++ src/main/java/com/benimhoff/portfolioWeb/controller/LoginController.java
@@ -1,14 +1,41 @@
 package com.benimhoff.portfolioWeb.controller;
 
+import com.benimhoff.portfolioWeb.service.PropietarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    PropietarioService propietarioService;
+
+    private boolean usuarioAutenticado = false;
+
     @GetMapping("/login")
-    public String verLogin(Model model){
+    public String verLogin() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String ingresar(@RequestParam("username") String username,
+            @RequestParam("password") String password, Model model) {
+       try {
+           propietarioService.esUsuarioValido(username, password);
+           usuarioAutenticado = true;
+           return "redirect:/homeEdicion";
+       } catch (IllegalArgumentException e){
+           model.addAttribute("error", e.getMessage());
+           return "login";
+       }
+    }
+
+    @GetMapping("/homeEdicion")
+    public String verHomeEdicion(){
+        return usuarioAutenticado ? "homeEdicion" : "redirect:/login";
     }
 }
