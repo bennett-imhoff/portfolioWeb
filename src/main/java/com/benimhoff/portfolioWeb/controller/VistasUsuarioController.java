b8466bf6bdbb16c546692160ec4251d1bd619aad
Bennett Imhoff Anzor <simplementebennett@gmail.com>
2024-01-12 18:27:36 -0300

    Agrego redes sociales al footer de la home para todos los usuarios

--- src/main/java/com/benimhoff/portfolioWeb/controller/VistasUsuarioController.java
+++ src/main/java/com/benimhoff/portfolioWeb/controller/VistasUsuarioController.java
@@ -1,11 +1,15 @@
 package com.benimhoff.portfolioWeb.controller;
 
 import com.benimhoff.portfolioWeb.domain.Propietario;
+import com.benimhoff.portfolioWeb.domain.RedSocial;
 import com.benimhoff.portfolioWeb.service.PropietarioService;
+import com.benimhoff.portfolioWeb.service.RedSocialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class VistasUsuarioController {
@@ -13,10 +17,15 @@ public class VistasUsuarioController {
     @Autowired
     private PropietarioService propietarioService;
 
+    @Autowired
+    private RedSocialService redSocialService;
+
     @GetMapping("/")
     public String home (Model model){
         Propietario propietario = propietarioService.ver();
+        List<RedSocial> redesSociales = redSocialService.verTodas();
         model.addAttribute("propietario", propietario);
+        model.addAttribute("redesSociales", redesSociales);
         return "home";
     }
 }
